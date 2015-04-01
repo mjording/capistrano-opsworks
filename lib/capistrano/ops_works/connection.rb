e500f4a5271e3ac85de7f05493b91fb59890a9ad
Matthew Jording <mjording@gmail.com>
2015-04-01 07:07:12 -0400

    use aws-sdk gem for deployment client

--- lib/capistrano/ops_works/connection.rb
+++ lib/capistrano/ops_works/connection.rb
@@ -8,7 +8,7 @@ module Capistrano
         access_key_id = aws.fetch(:access_key_id)
         secret_access_key = aws.fetch(:secret_access_key)
 
-        @client = AWS::OpsWorks.new(\
+        @client = Aws::OpsWorks::Client.new(region:'us-east-1',
           :access_key_id => access_key_id,
           :secret_access_key => secret_access_key).client
 
